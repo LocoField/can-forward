6265cb46e5742966ddf66e36720965b2d769da93
LocoField <aiesko@locofield.com>
2025-02-28 18:18:49 +0900

    fix log file bug

--- can-forward.py
+++ can-forward.py
@@ -9,26 +9,33 @@ fwd_data_filepath = root_dir + 'fwd.data'
 can0_log_filepath = root_dir + 'can0.log'
 can1_log_filepath = root_dir + 'can1.log'
 
+if os.path.exists(can0_log_filepath):
+    os.remove(can0_log_filepath)
+
+if os.path.exists(can1_log_filepath):
+    os.remove(can1_log_filepath)
+
 adp_data_dict = pickle.load(open(fwd_data_filepath, 'rb'))
 
-can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan')
-can1 = can.interface.Bus(channel = 'can1', bustype = 'socketcan')
+can0 = can.interface.Bus(channel = 'can0', interface = 'socketcan')
+can1 = can.interface.Bus(channel = 'can1', interface = 'socketcan')
 
 can0_queue = queue.Queue()
 can1_queue = queue.Queue()
 
 def process_can0():
     while True:
-        with open(can0_log_filepath, 'wb') as log_file:
-            msg = can0.recv(0.1)
-            if msg is not None:
+        msg = can0.recv()
+        if msg is not None:
+            with open(can0_log_filepath, 'a') as log_file:
                 can0_queue.put(msg)
-                
+
                 log_file.write(f"<< {msg}\n")
                 log_file.flush()
 
-            fwd = can1_queue.get()
-            if fwd is not None:
+        fwd = can1_queue.get()
+        if fwd is not None:
+            with open(can0_log_filepath, 'a') as log_file:
                 can0.send(fwd)
 
                 log_file.write(f">> {fwd}\n")
@@ -58,13 +65,13 @@ def process_can1():
                     check=False
                     )
 
-        with open(can1_log_filepath, 'wb') as log_file:
+        with open(can1_log_filepath, 'a') as log_file:
             can1.send(msg)
 
             log_file.write(f">> {msg}\n")
             log_file.flush()
-        
-            ack = can1.recv(0.1)
+
+            ack = can1.recv()
             if ack is not None:
                 can1_queue.put(ack)
 
